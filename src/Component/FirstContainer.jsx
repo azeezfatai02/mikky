88935a849afb74bc6fc894ed4c104a45b28ee6b1
azeezfatai02 <azeezfatai02@gmail.com>
2024-08-20 17:41:40 +0100

    all up

--- src/Component/FirstContainer.jsx
+++ src/Component/FirstContainer.jsx
@@ -4,27 +4,11 @@ export default function FirstContainer() {
   return (
     <div className="theContainer">
       <div className="firstcont">
-        {/* <div className="fsetimg">
-          <div className="hero_img">
-            <img src="./battery.png" alt="" />
-          </div>
-          <div className="hero_img">
-            <img src="./battery.png" alt="" />
-          </div>
-        </div> */}
         <div className="middlealfa">
           <h5>Welcome to</h5>
           <h1>MIKKY NIG ENTERPRISE</h1>
           <h3>Dealer in all kinds of motor spare part</h3>
         </div>
-        {/* <div className="fsetimg">
-          <div className="hero_img">
-            <img src="./battery.png" alt="" />
-          </div>
-          <div className="hero_img">
-            <img src="./battery.png" alt="" />
-          </div>
-        </div> */}
       </div>
     </div>
   );
