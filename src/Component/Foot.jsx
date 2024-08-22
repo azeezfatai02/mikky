8953c4ab4db2e7285fac48075e439555ff9f7266
azeezfatai02 <azeezfatai02@gmail.com>
2024-08-22 17:13:37 +0100

    foot

--- src/Component/Foot.jsx
+++ src/Component/Foot.jsx
@@ -1,17 +1,24 @@
 import Link from "next/link";
+import "./Foot.css";
 
 export default function Foot() {
   return (
     <footer>
-      <div className="allfoot">
-        <div>
+      <div className="footer-container">
+        <div className="footer-logo">
           <h1>MIKKY NIG ENTERPRISE</h1>
+          <p>123 Business St, Lagos, Nigeria</p>
+          <p>Email: contact@mikkynig.com</p>
+          <p>Phone: +234 806 678 1722</p>
         </div>
 
-        <div className="footlink">
-          <Link href="/home">Home</Link>
-
-          <Link href="/product">Product</Link>
+        <div className="footer-links">
+          <Link href="/home" aria-label="Home Page">
+            Home
+          </Link>
+          <Link href="/product" aria-label="Product Page">
+            Product
+          </Link>
         </div>
       </div>
     </footer>
